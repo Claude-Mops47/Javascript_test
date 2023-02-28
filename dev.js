4afdb55a069bb20fbdf12e8805db412cc072678b
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-02-28 19:15:42 +0100

    ok

--- dev.js
+++ dev.js
@@ -1,18 +1,16 @@
-const emp = {name: "Claude"};
+const emp = { name: "Claude" };
 
 Object.seal(emp);
 
-try{
-
-Object.defineProperty(emp, 'bonus', {value: 3500});
-}
-catch{
-console.log("can not define new property");
+try {
+  Object.defineProperty(emp, "bonus", { value: 3500 });
+} catch {
+  console.log("can not define new property");
 }
 
 emp.name = "Emmanuel Mopeno";
 
 console.log(Object.isSealed(emp));
 
-console.log(delete emp.name)
-console.log(emp)
+console.log(delete emp.name);
+console.log(emp);
