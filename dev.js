50e793debd141523f28cf435aa8e7d287283526a
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-02-28 18:00:02 +0100

    dev .js

---
+++ dev.js
@@ -0,0 +1,18 @@
+const emp = {name: "Claude"};
+
+Object.seal(emp);
+
+try{
+
+Object.defineProperty(emp, 'bonus', {value: 3500});
+}
+catch{
+console.log("can not define new property");
+}
+
+emp.name = "Emmanuel Mopeno";
+
+console.log(Object.isSealed(emp));
+
+console.log(delete emp.name)
+console.log(emp)
