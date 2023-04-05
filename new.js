c77321560943c9dee2a856238cd2dff4bac06b5c
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-04-05 12:27:34 +0000

    new

--- new.js
+++ new.js
@@ -1,4 +1,5 @@
-const a = " HELLO MORNINGSTAR ";
+const a =
+  " HELLO MORNINGSTAR, Notez que le code JavaScript utilise setTimeout pour simuler l'effet de la fonction Sleep de Windows.h. De plus, il utilise process.stdout.write plutôt que console.log pour permettre l'impression des caractères sur la même ligne. ";
 const n = a.length;
 
 for (let i = 0; i < n; i++) {
