1f5a9590f3762799ad95fc55819eef11308496b5
Claude-Mops47 <mopenobiaclaude@gmail.com>
2023-04-08 17:49:45 +0000

    add caption

--- new.js
+++ new.js
@@ -1,27 +1,53 @@
-const a =
-  " HELLO MORNINGSTAR, Notez que le code JavaScript utilise setTimeout pour simuler l'effet de la fonction Sleep de Windows.h. De plus, il utilise process.stdout.write plutôt que console.log pour permettre l'impression des caractères sur la même ligne. ";
-const n = a.length;
+// const a =
+//   " HELLO MORNINGSTAR, Notez que le code JavaScript utilise setTimeout pour simuler l'effet de la fonction Sleep de Windows.h. De plus, il utilise process.stdout.write plutôt que console.log pour permettre l'impression des caractères sur la même ligne. ";
+// const n = a.length;
 
-for (let i = 0; i < n; i++) {
-  setTimeout(() => {
-    process.stdout.write(a[i]);
-  }, i * 100);
+// for (let i = 0; i < n; i++) {
+//   setTimeout(() => {
+//     process.stdout.write(a[i]);
+//   }, i * 100);
+// }
+
+// //
+// const video = document.querySelector("video");
+// const constraints = {
+//   audio: false,
+//   video: {
+//     facingMode: "user",
+//   },
+// };
+
+// navigator.mediaDevices
+//   .getUserMedia(constraints)
+//   .then((stream) => {
+//     video.srcObject = stream;
+//   })
+//   .catch((error) => {
+//     console.error("Error accessing camera: ", error);
+//   });
+
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const headerMenu = document.getElementById("header");
+
+if (burgerMenu && navbarMenu) {
+  burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("is-active");
+    navbarMenu.classList.toggle("is-active");
+  });
 }
 
-//
-const video = document.querySelector("video");
-const constraints = {
-  audio: false,
-  video: {
-    facingMode: "user",
-  },
-};
-
-navigator.mediaDevices
-  .getUserMedia(constraints)
-  .then((stream) => {
-    video.srcObject = stream;
-  })
-  .catch((error) => {
-    console.error("Error accessing camera: ", error);
+document.querySelectorAll(".menu-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    burgerMenu.classList.remove("is-active");
+    navbarMenu.classList.remove("is-active");
   });
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 769) {
+    if (navbarMenu.classList.contains("is-active")) {
+      navbarMenu.classList.remove("is-active");
+    }
+  }
+});
